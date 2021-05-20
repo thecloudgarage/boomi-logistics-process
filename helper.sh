f3359fc5dde835985452321859a6764bc09c3fd2
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2021-05-20 18:22:06 +0530

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -33,3 +33,13 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 sudo apt-get update -y
 sudo apt-get install awscli -y
+
+#INSTALL NODEJS
+sudo su
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh -o install_nvm.sh
+bash install_nvm.sh
+source ~/.profile
+nvm install v14.17.0
+nvm use v14.17.0
+npm config set user 0
+npm config set unsafe-perm true
